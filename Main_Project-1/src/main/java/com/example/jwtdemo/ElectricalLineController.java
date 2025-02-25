a9d3bc2fa9df2831ca620bf45b4a7705eeff1941
GustavoLugo001 <105478989+GustavoLugo001@users.noreply.github.com>
2025-02-25 15:50:16 -0800

    Update ElectricalLineController.java

--- Main_Project-1/src/main/java/com/example/jwtdemo/ElectricalLineController.java
+++ Main_Project-1/src/main/java/com/example/jwtdemo/ElectricalLineController.java
@@ -6,9 +6,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
-
+//RequestMapping will allow them to be called to be called by the api.js, however, it must pass the securityFIlter in SecurityConfig.
 @RestController
-@RequestMapping("/api/electrical-lines")
+@RequestMapping("/api/electrical-lines") //the directory to call each function have addtional in order to make more specific calls.
 public class ElectricalLineController {
 
     @Autowired
@@ -16,6 +16,8 @@ public class ElectricalLineController {
     
     @Autowired
     private UserRepository userRepository;
+
+    //approval is more for ROLE_USER to allow ROLE_ADMIN the ability to approve.
     
     @PutMapping("/approve/{id}")
     public ResponseEntity<String> approveElectricalLine(@PathVariable Long id) {
@@ -37,6 +39,8 @@ public class ElectricalLineController {
         return ResponseEntity.ok("Electrical line approved successfully.");
     }
 
+    //Deny is more for ROLE_USER to allow ROLE_ADMIN the ability to deny.
+    
     @PutMapping("/deny/{id}")
     public ResponseEntity<String> denyElectricalLine(@PathVariable Long id) {
         String username = SecurityContextHolder.getContext().getAuthentication().getName();
@@ -58,7 +62,7 @@ public class ElectricalLineController {
     }
 
 
-    
+    //This allows the ability to get the ElectricalLine  based on the user based ont he admin as well.
     @GetMapping
     public ResponseEntity<List<ElectricalLine>> getElectricalLines() {
         String username = SecurityContextHolder.getContext().getAuthentication().getName();
@@ -74,7 +78,9 @@ public class ElectricalLineController {
         List<ElectricalLine> lines = electricalLineRepository.findByAdminId(adminId);
         return ResponseEntity.ok(lines);
     }
-    
+
+    //This allows to add of ElecricalLine to be added when using the front-end this allows the communication.
+    //it also has teh feature of allowing PENDING for ROLE_USER and APPROVAL for ROLE_ADMIN
     @PostMapping
     public ResponseEntity<ElectricalLine> addElectricalLine(@RequestBody ElectricalLine electricalLine) {
         String username = SecurityContextHolder.getContext().getAuthentication().getName();
@@ -92,7 +98,7 @@ public class ElectricalLineController {
         ElectricalLine savedLine = electricalLineRepository.save(electricalLine);
         return ResponseEntity.ok(savedLine);
     }
-    
+    //based on the ID the WaterLine will be deleted.
     @DeleteMapping("/{id}")
     public ResponseEntity<?> deleteElectricalLine(@PathVariable Long id) {
         String username = SecurityContextHolder.getContext().getAuthentication().getName();
